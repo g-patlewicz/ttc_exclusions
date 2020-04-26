5dcd3b27496255deb6cb6a3bdb25174bee0b0b28
patlewig <patlewig@users.noreply.github.com>
2020-04-26 17:01:10 -0400

    Initial commit

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name='src',
+    packages=find_packages(),
+    version='0.1.0',
+    description='Investigation of the feasibility of refining exclusion rules within the Kroes et al (2004) workflow for TTC.',
+    author='patlewig',
+    license='MIT',
+)
